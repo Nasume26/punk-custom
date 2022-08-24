9d7f834eaf498ab81dcc635eb250fc4fad5fba35
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-08-24 16:37:42 -0500

    Can connect to DB can not update

--- controllers/beersController.js
+++ controllers/beersController.js
@@ -1,22 +1,60 @@
 import beersJSON from '../beersJS.json' assert {type: 'json'}
-import sequelize from 'sequelize';
+import sequelize, { DataTypes } from 'sequelize';
 
 
 const { Sequelize } = sequelize; 
 
-const sequelizer = new Sequelize('database', 'root', 'password', {
+const sequelizer = new Sequelize('beers', 'root', 'password', {
     host:'localhost',
     dialect:'mysql'
 });
 
 
+try {
+    await sequelizer.authenticate();
+    console.log("Succesfully connected to MYSQL Database")
+} catch (error) {
+    console.log('Unable to connect to Database', error)
+}
 
 
+const beer = sequelizer.define('beer', {
+    id: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       primaryKey: true
+}, name: {
+    type: DataTypes.STRING
+}, tagline: {
+    type: DataTypes.STRING
+}, first_brewed: {
+    type: DataTypes.STRING
+}, description: {
+    type: DataTypes.STRING
+}, image_url: {
+    type: DataTypes.STRING
+}, abv: {
+   type: DataTypes.INTEGER
+}, ph: {
+    type: DataTypes.INTEGER
+}
 
+})
 
+const beerFunction = (id, name, tagline, first_brewed, description, image_url, abv, ph) => {
+    const newBeer = beer.build({
+        id: id,
+        name: name,
+        tagline: tagline,
+        first_brewed: first_brewed,
+        description: description,
+        image_url: image_url,
+        abv: abv,
+        ph: ph 
+    })
+    newBeer.save();
 
-
-
+}
 
 const beers = beersJSON;
 
@@ -36,16 +74,18 @@ export const postBeers = (req, res) => {
     const abv = req.body.abv
     const ph = req.body.ph
     
-    beers.push({
-        id: id,
-        name: name,
-        tagline: tagline,
-        first_brewed: first_brewed,
-        description: description,
-        image_url: image_url,
-        abv: abv,
-        ph: ph 
-    })
+    // beers.push({
+        // id: id,
+        // name: name,
+        // tagline: tagline,
+        // first_brewed: first_brewed,
+        // description: description,
+        // image_url: image_url,
+        // abv: abv,
+        // ph: ph 
+    // })
+
+    beerFunction(id, name, tagline, first_brewed, description, image_url, abv, ph)
 
     res.status(200).send(
         beers
